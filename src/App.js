313d98902e1a207d54ced408123d77509e17e62c
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-03-26 07:41:30 +0600

    react files download completed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello World!</h2>
+      <p>I'm Mamun</p>
+      <p>I'm ready for Assignment 8</p>
+    </div>
+  );
+}
+
+export default App;
